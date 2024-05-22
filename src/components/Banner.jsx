c36fa8a3b9607b84e5408e12cdb12eeec7ebf66f
kushalkumarghosh <ghoshkushalcse@gmail.com>
2024-05-22 21:59:13 +0600

    updated the banner section

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,6 +3,8 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 import img1 from "../assets/Banner/recipe1.png";
 import img2 from "../assets/Banner/recipe2.png";
+import img3 from "../assets/Banner/recipe3.png";
+import img4 from "../assets/Banner/recipe4.png";
 
 const Banner = () => {
     return (
@@ -12,6 +14,12 @@ const Banner = () => {
             </div>
             <div>
                 <img src={img2} />
+            </div>
+            <div>
+                <img src={img3} />
+            </div>
+            <div>
+                <img src={img4} />
             </div>
         </Carousel>
     );
